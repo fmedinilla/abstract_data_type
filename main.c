5233c7561968885fe5902c1d8f2965e1e05aaf52
Francisco Medinilla Garcia <medinillagarciafrancisco@gmail.com>
2024-01-20 16:41:09 +0100

    empty main.c

--- main.c
+++ main.c
@@ -3,26 +3,6 @@
 
 int main(void)
 {
-    tSimpleList list;
-    simple_list_init(&list);
-
-    simple_list_insert(&list, 0, 1);
-    simple_list_insert(&list, 1, 2);
-    simple_list_insert(&list, 2, 3);
-    simple_list_insert(&list, 3, 4);
-    simple_list_insert(&list, 7, 5);
-
-    simple_list_delete(&list, 1);
-    simple_list_delete(&list, 1);
-
-    int pos = 0;
-    int value;
-    while (!simple_list_is_end(list, pos)) {
-        simple_list_get(list, pos, &value);
-        printf("Value at %d: %d\n", pos, value);
-        pos++;
-    }
-
-    printf("List len: %d\n", simple_list_length(list));
+    printf("Abstract Data Types\n");
     return 0;
 }
